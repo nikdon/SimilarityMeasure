28d1298c36ebc7c256b3ef690f6c4bbb9e3cd68b
nik_do <nd.startup@gmail.com>
2014-03-31 09:44:00 +0400

    Clear code and comments

--- SimilarityMeasure/Program.cs
+++ SimilarityMeasure/Program.cs
@@ -18,29 +18,30 @@ namespace SimilarityMeasure
             string url3 = "http://www.dailymail.co.uk/sport/cricket/article-2591835/Pietersen-adds-Caribbean-Premier-League-packed-diary-gun-hire-enters-tournament-draft.html";
             string url4 = "http://www.telegraph.co.uk/sport/cricket/kevinpietersen/10730271/Kevin-Pietersen-enters-Caribbean-Premier-League-draft.html";
 
-            string testUrl1 = url1;
-            string testUrl2 = url2;
-
             int treshold = 3;
-
 
             try
             {
+                // Create instance of calculator
                 SimilarityCalculator sc = new SimilarityCalculator();
 
-                // Comparison of articles about minister (same)
+                // Compare atricles at url1 and url2
                 Console.WriteLine("Comparison of articles about minister (similar articles from dailymail.co.uk and telegraph.co.uk)..");
                 sc.Compare(url1, url2, vocabularyTreshold: treshold);
 
+                // Compare atricles at url3 and url4
                 Console.WriteLine("Comparison of articles about cricket (similar articles from dailymail.co.uk and telegraph.co.uk)..");
                 sc.Compare(url3, url4, vocabularyTreshold: treshold);
 
+                // Compare atricles at url1 and url3
                 Console.WriteLine("Comparison of article about minister and article about cricket (source - dailymail.co.uk)..");
                 sc.Compare(url1, url3, vocabularyTreshold: treshold);
 
+                // Compare atricles at url2 and url4
                 Console.WriteLine("Comparison of article about minister and article about cricket (source - telegraph.co.uk)..");
                 sc.Compare(url2, url4, vocabularyTreshold: treshold);
 
+                // Compare atricles at url1 and url4
                 Console.WriteLine("Comparison of article about minister and article about cricket (source - telegraph.co.uk and dailymail.co.uk)..");
                 sc.Compare(url1, url4, vocabularyTreshold: treshold);
 
